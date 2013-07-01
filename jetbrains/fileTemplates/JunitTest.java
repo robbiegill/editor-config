42113b4a997fb9463b12d0537034c21f08ce5bf7
Robbie <rgill@alpinedatalabs.com>
2013-06-30 17:44:18 -0700

    intellij templates

---
+++ jetbrains/fileTemplates/JunitTest.java
@@ -0,0 +1,11 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+public class ${NAME} {
+    
+}
